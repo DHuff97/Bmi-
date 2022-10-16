7ad6eb4838e1d71340727c275f9d3952f4a6f959
DHuff97 <young.huff97@gmail.com>
2022-10-16 18:53:47 -0400

    updated prompts and output

--- bmiimperial.py
+++ bmiimperial.py
@@ -2,8 +2,8 @@ weight = float(0.453592)
 height = float(0.0254)
 
 
-Weight_input = float(input("How much do you weigh?"))
-Height_input = float(input("How tall are you?"))
+Weight_input = float(input("Please enter weight in pounds: "))
+Height_input = float(input("Please enter height in inches: "))
 
 
 #print(f"Your weight is  {Weight_input * weight}")
@@ -18,7 +18,7 @@ numberh = float(Height_input * height)
 bmi = (numberw / numberh**2)
 
 tbmi = bmi
-print(tbmi)
+print(f"bmi is: {tbmi}")
 
 
 
